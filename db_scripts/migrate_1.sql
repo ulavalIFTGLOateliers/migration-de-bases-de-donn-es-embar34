05994d13859246bc8969f3efe8b9905232d7d6e0
Emilie Barrette <embar34@ulaval.ca>
2024-03-25 13:44:53 -0400

    Commit_vf

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,5 +1,4 @@
 ALTER TABLE singer RENAME TO musician;
-<<<<<<< HEAD
 ALTER TABLE musician CHANGE COLUMN singerName musicianName VARCHAR(50);
 ALTER TABLE musician ADD COLUMN role VARCHAR(50);
 ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
@@ -30,8 +29,3 @@ INSERT INTO band (bandName, creation, genre) VALUES
 ('Crazy Duo', 2015, 'rock'),
 ('Luna', 2009, 'classical'),
 ('Mysterio', 2019, 'pop');
-=======
-CREATE TABLE band (bandName varchar(50) PRIMARY KEY, creation YEAR, genre varchar(50));
-INSERT INTO band (bandName, creation, genre) VALUES ('Crazy Duo','2015','rock'), ('Luna','2009','classical'),
-                                                    ('Mysterio','2019','pop' );
->>>>>>> 67a8871e8cd7bba3e6fdd1a10a0d97afa8b675b1
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 DROP TABLE band;
 ALTER TABLE musician DROP COLUMN role;
 ALTER TABLE musician DROP COLUMN bandName;
 ALTER TABLE musician CHANGE COLUMN musicianName singerName VARCHAR(50);
 ALTER TABLE musician RENAME TO singer;
-=======
-ALTER TABLE musician RENAME TO singer;
-DROP TABLE band;
->>>>>>> 67a8871e8cd7bba3e6fdd1a10a0d97afa8b675b1
